55ddedc0b281bc4e45c2d194b3c35ac8937b8f19
Nicholas <nicholas.isikhueme@outlook.com>
2026-03-29 07:26:47 +0200

    yes

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,15 @@
 <?php
 
-$userName = "root";
-$hostName = "localhost";
-$password = "";
-$dbName = "ebewd2_cr5_animal_adoption_nicholas";
+$userName = "if0_41499403";       
+$hostName = "sql211.infinityfree.com";
+$password = "Malcolmx0147";
+$dbName = "if0_41499403_ebewd2_cr5_animal_adoption_nicholas";
 
 $conn = mysqli_connect($hostName, $userName, $password, $dbName);
 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 function cleanInputs($value)
 {
@@ -16,5 +19,4 @@ function cleanInputs($value)
 
     return $data;
 }
-
-
+?>
